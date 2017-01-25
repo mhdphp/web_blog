ff74232251cd1b83f5ce2176d97c60e1baaf2db6
Mihai <mihai32@gmail.com>
2017-01-25 17:59:17 +0200

    src/app.py // src/static/css/main.css // src/templates/base.html // src/templates/login.html //

--- src/app.py
+++ src/app.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+from src.models.user import User
+
 
 # tell python we are creating a flask application
 # create a class of Flask
 app = Flask(__name__) # __main__
 
-# the end point will the forward slash after the api/
-@app.route('/') # www.mysite/api/
-
-# define a method that executes this end poit
+# define the first end-point
+@app.route('/')
 def hello_world():
-    return "Hello World...Romania"
+    # return 'Hello, world'
+    return render_template('login.html')
+
+
+# define another end-point
+@app.route('/login')
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    # check if email,and password match
+    if User.login_valid(email, password):
+        # if matched save the email in session
+        User.login(email)
+
+    # redirect the user on a profile.html page, with email on the session saved
+    return render_template('profile.html', email = session['email'])
+
+
 
 # in order to work this app
 if __name__ == '__main__':
