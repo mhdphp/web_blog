a468b1fa2ef4f03418b3e2d5a5bac980a41f82ef
Mihai <mihai32@gmail.com>
2017-01-26 07:41:47 +0200

    app.py, base.html, login.html, profile.html

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, session
+
+from src.common.database import Database
 from src.models.user import User
 
 
 # tell python we are creating a flask application
 # create a class of Flask
-app = Flask(__name__) # __main__
+app = Flask(__name__)
+# create a secret_key for session to be able to work
+app.secret_key = "adfaDSADFa233r423sdfa"
+
 
 # define the first end-point
 @app.route('/')
@@ -13,8 +18,15 @@ def hello_world():
     return render_template('login.html')
 
 
+# we have to initialize the database
+# use Flask dec
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
 # define another end-point
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
@@ -25,13 +37,12 @@ def login_user():
         User.login(email)
 
     # redirect the user on a profile.html page, with email on the session saved
-    return render_template('profile.html', email = session['email'])
-
+    return render_template('profile.html', email=session['email'])
 
 
 # in order to work this app
 if __name__ == '__main__':
-    app.run()
-    # we could other port
+    app.run(port=4995)
+    # we could set other port
     # app.run(port=4000)
 
